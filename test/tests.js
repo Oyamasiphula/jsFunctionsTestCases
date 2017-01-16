7ac67dd16aa8351dab2596ee26b2579fcd039700
oyamasiphula <oyama@projectcodex.co>
2017-01-16 15:35:05 +0200

    now managed to create 2 tests the one thats passing and the other which is failing

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const multiplicationOf2Dig = require("../multiplyThem");
 
 describe("Javascript continous intergration", function() {
-    it('Should test for the multiplication function and should intentionally fail', function() {
+    it('Should test for the multiplication function and should intentionally pass', function() {
         assert.equal(6, multiplicationOf2Dig.multiplyThem(3, 1));
     });
     it('Should test for the multiplication function should intentionally pass', function() {
