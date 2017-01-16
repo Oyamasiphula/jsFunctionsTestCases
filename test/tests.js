c668db76444a42e28753cfc7220fad3ee9fbfe8e
oyamasiphula <oyama@projectcodex.co>
2017-01-16 16:09:12 +0200

    all tests are passing

--- test/tests.js
+++ test/tests.js
@@ -3,8 +3,8 @@ const multiplicationOf2Dig = require("../multiplyThem");
 
 describe("Javascript continous intergration", function() {
     it('Should test for the multiplication function and should intentionally pass', function() {
-        assert.equal(6, multiplicationOf2Dig.multiplyThem(3, 2));
-    });
+        assert.equal(20, multiplicationOf2Dig.multiplyThem(10, 2));
+    })
     it('Should test for the multiplication function should intentionally pass', function() {
         assert.equal(6, multiplicationOf2Dig.multiplyThem(3, 2));
     });
