62014198ac22f60e4ca5078368b6d6ec3bd7aade
oyamasiphula <oyama@projectcodex.co>
2017-01-31 22:51:12 +0200

    added more test

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,22 @@
 const assert = require("assert");
 const multiplicationOf2Dig = require("../multiplyThem");
+const addThem = require("../paramsDemo");
+const advancedFunctUsage = require("../advancedFunctions");
 
-describe("Javascript continous intergration", function() {
+describe("Javascript continous intergration ", function() {
     it('Should test for the multiplication function and should intentionally pass', function() {
         assert.equal(20, multiplicationOf2Dig.multiplyThem(10, 2));
     })
     it('Should test for the multiplication function should intentionally pass', function() {
         assert.equal(6, multiplicationOf2Dig.multiplyThem(3, 2));
+    });
+    it('Should test for the function parameter"s addition', function() {
+        assert.equal(3, addThem.theParamFunction(1,2));
+    });
+    it('Should test for the first function`s end result', function() {
+        assert.equal("This is funcOne`s result", advancedFunctUsage.funcOne("This is funcOne`s result"));
+    });
+    it('Should test for the first function`s and 2nd function to pass function1`s result', function() {
+        assert.equal("This is funcOne`s result", advancedFunctUsage.execFunc(funcOne));
     });
 });
