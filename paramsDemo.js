cd5e5da1acb246c127e854b59f66c1f2bca7404c
oyamasiphula <oyama@projectcodex.co>
2017-01-31 23:51:54 +0200

    added a function params intro

---
+++ paramsDemo.js
@@ -0,0 +1,15 @@
+//execute this code in the editor below
+
+//an empty function called 'theFunction'
+var theFunction = function(){
+    // do something here
+    // return a result if needed
+}
+
+//an function called 'theParamFunction'
+exports.theParamFunction = function(num1, num2){
+    //the function logic
+    var result = num1 + num2;
+    //return parameters
+    return result;
+};
