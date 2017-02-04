84f7fd42782a109132c1cbd1d4e227cc382741d1
oyamasiphula <oyama@projectcodex.co>
2017-02-04 23:59:26 +0200

    added a test that passing

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,9 @@
 const assert = require("assert");
 const multiplicationOf2Dig = require("../multiplyThem");
 const addThem = require("../paramsDemo");
-const advancedFunctUsage = require("../advancedFunctions");
+const checkRegitrationNumber = require("../advancedFunctions");
+const advancedFunctUsage = require("../checkRegitrationNumber");
+
 
 describe("Javascript continous intergration ", function() {
     it('Should test for the multiplication function and should intentionally pass', function() {
@@ -11,12 +13,15 @@ describe("Javascript continous intergration ", function() {
         assert.equal(6, multiplicationOf2Dig.multiplyThem(3, 2));
     });
     it('Should test for the function parameter"s addition', function() {
-        assert.equal(3, addThem.theParamFunction(1,2));
+        assert.equal(3, addThem.theParamFunction(1, 2));
     });
     it('Should test for the first function`s end result', function() {
         assert.equal("This is funcOne`s result", advancedFunctUsage.funcOne("This is funcOne`s result"));
     });
     it('Should test for the first function`s and 2nd function to pass function1`s result', function() {
         assert.equal("This is funcOne`s result", advancedFunctUsage.execFunc(advancedFunctUsage.funcOne));
+    });
+    it('Should test for the checkPlateNo (isFromBellville', function() {
+        assert.equal("This is funcOne`s result", advancedFunctUsage.advancedFunctUsage(advancedFunctUsage.funcOne));
     });
 });
