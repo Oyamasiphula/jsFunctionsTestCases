4fa1a6d48f7720fbcdb0e5e4ac9bc0d2f136435f
oyamasiphula <oyama@projectcodex.co>
2017-02-05 23:32:18 +0200

    added tests for functions

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,8 @@ const advancedFunctUsage = require("../advancedFunctions");
 const chekRegistrationNumber = require("../chekRegistrationNumber");
 const differentScopes = require("../differentScopes");
 const declaringVScalling = require("../declaringVScalling");
+const greet = require("../greet");
+const isFromLocation = require("../isFromLocation");
 
 
 describe("Javascript continous intergration ", function() {
@@ -34,5 +36,15 @@ describe("Javascript continous intergration ", function() {
     });
     it('Should test for the function creating vs done then execute to get an output as a typeof(value) of a function ', function() {
         assert.equal("string", typeof(declaringVScalling.theFunction()));
+    });
+    it('should test for the greet function and should be able to greet anyone 1 param use only', function() {
+        assert.equal("Hello, Janine", greet.greet("Janine"));
+    });
+    it('should test for reg check CA and should be able to return false/true with the use of 1 param only but diff data', function() {
+        assert.equal(true, isFromLocation.isFromCapeTown("CA 123 908"));
+    });
+    it('should test for reg check CA and should be able to return false/true with the use of 1 param only, but diff data should be different', function() {
+        assert.equal(false, isFromLocation.isFromCapeTown("CJ 123 908"));
+
     });
 });
