1a9457d0c18326ee7f439cbd451ecab00bbc40bb
oyamasiphula <oyama@projectcodex.co>
2017-02-05 22:30:06 +0200

    now removed a file which I mistakenly mispelt its name

--- chekRegitrationNumber.js
+++
@@ -1,7 +0,0 @@
-var isFromBellville = function(plateNumber){
-  console.log(plateNumber);
-  var checkPlateNo = plateNumber.startsWith("CY");
-  return checkPlateNo;
-  }
-
-isFromBellville("CY 123")
