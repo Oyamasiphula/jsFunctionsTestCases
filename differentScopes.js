22898f39d4c9e2eb12ab74d93b27b06ea072ad26
oyamasiphula <oyama@projectcodex.co>
2017-02-05 22:38:04 +0200

    added function that uses global vars... and returns their values conc with some str

---
+++ differentScopes.js
@@ -0,0 +1,12 @@
+var name = "David Blankit";
+
+var favouriteSport = "Golf";
+
+var favSport = function(){
+console.log(name + "'s favourate sport is " + favouriteSport);
+return name + "'s favourate sport is " + favouriteSport;
+
+};
+exports.favSport = favSport;
+
+console.log(favouriteSport);
