faa5b1b04326f5b875f81bfa1cae6ca1942cb690
oyamasiphula <oyama@projectcodex.co>
2017-02-05 22:31:09 +0200

    added function that can check for Bellville plates only

---
+++ chekRegistrationNumber.js
@@ -0,0 +1,6 @@
+var isFromBellville = function(plateNumber){
+  console.log(plateNumber);
+  var checkPlateNo = plateNumber.startsWith("CY");
+  return checkPlateNo;
+  }
+exports.isFromBellville = isFromBellville; 
